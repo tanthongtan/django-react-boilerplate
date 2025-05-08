170695e228fb8a2e3c13005b5db47b81c0b474af
tanthongtan <tan.thongtan1@gmail.com>
2025-05-09 01:03:22 +0700

    typescript support + build script + strict mode || for django, react, css, css modules, typescript boilerplate start here ||

---
+++ frontend/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from "react-dom/client";
+
+const rootElement = document.getElementById("root")
+if (!rootElement) throw new Error('Root element not found');
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <h1>Hello, react!</h1>
+  </React.StrictMode>
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,31 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
-  entry: './frontend/index.js',  // path to our input file
+  entry: './frontend/index.tsx',  // path to our input file
   output: {
     filename: 'index-bundle.js',  // output bundle file name
     path: path.resolve(__dirname, './static'),  // path to our Django static directory
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
   },
   module: {
     rules: [
       {
         oneOf: [
           {
-            test: /\.(js|jsx)$/,
+            test: /\.(js|jsx|ts|tsx)$/,
             exclude: /node_modules/,
             loader: "babel-loader",
-            options: { presets: ["@babel/preset-env", ["@babel/preset-react", { runtime: "automatic" }]] }
+            options: {
+              presets: [
+                "@babel/preset-env",
+                ["@babel/preset-react", { runtime: "automatic" }],
+                "@babel/preset-typescript"
+              ]
+            }
           },
           {
             test: /\.css$/,
@@ -43,6 +53,7 @@ module.exports = {
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'index-bundle.css',
-    })
+    }),
+    new ForkTsCheckerWebpackPlugin()
   ],
 };
