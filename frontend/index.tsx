9db94a1c2ec1322c36a5ccbdb6234366866e595a
tanthongtan <tan.thongtan1@gmail.com>
2025-06-18 22:49:09 +0700

    css reset

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
 
+import "./index.css";
+
 const rootElement = document.getElementById("root")
 if (!rootElement) throw new Error('Root element not found');
 const root = ReactDOM.createRoot(rootElement);
